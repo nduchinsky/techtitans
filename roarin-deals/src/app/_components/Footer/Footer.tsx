d7098f2a343a1f659f4026f19234d4c5a34d18e1
Nathanial Duchinsky <127921416+nduchinsky@users.noreply.github.com>
2024-10-03 20:19:10 -0500

    Revert "updating branch "

--- roarin-deals/src/app/_components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Footer.module.scss';
-
-export default function Footer(){
-    return(
-        <div className={styles.footerContainer}>
-        </div>
-    );
-}
--- roarin-deals/src/app/_components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.scss';
-
-export default function Header(){
-    return(
-        <div className={styles.headerContainer}>
-            <h1 className={styles.headerText}>Roarin' Deals</h1>
-        </div>
-    );
-}
