379598a52b07cea38db5658bcbe2f33d6e27a427
Megan Murphy <mlmurphy1124@gmail>
2024-10-06 19:07:58 -0500

    login button working correctly

--- roarin-deals/src/app/_components/Header/HeaderButtons/HeaderButtons.tsx
+++ roarin-deals/src/app/_components/Header/HeaderButtons/HeaderButtons.tsx
@@ -1,9 +1,24 @@
+"use client"
+
+import Link from 'next/link';
 import styles from './HeaderButtons.module.scss'
+import { useRouter } from 'next/navigation';
 
 export const HeaderButtons = () => {
+
+    const router = useRouter();
+
+    const handleLoginClick = () => {
+      router.push('/login');
+    };
+  
+    const handleRegisterClick = () => {
+      router.push('/register');
+    };
+
     return(
         <div className={styles.buttonContainer}>
-            <button className={styles.loginButton}>Login</button>
+            <button className={styles.loginButton} onClick={handleLoginClick}>Login</button>
             <button className={styles.registerButton}>Register</button>
         </div>
     );
