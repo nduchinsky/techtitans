49c905c28c5829c832942e54599222df3712c0a3
Megan Murphy <mlmurphy1124@gmail>
2024-10-07 00:14:47 -0500

    added public directory, and refactored header for image

--- roarin-deals/src/app/_components/Header/Header.tsx
+++ roarin-deals/src/app/_components/Header/Header.tsx
@@ -3,7 +3,7 @@
 import styles from './Header.module.scss';
 import { HeaderButtons } from './HeaderButtons/HeaderButtons';
 import { useRouter } from 'next/navigation';
-
+import Image from 'next/image';
 
 export default function Header(){
     const router = useRouter();
@@ -14,6 +14,13 @@ export default function Header(){
 
     return(
         <div className={styles.headerContainer}>
+            <Image 
+                src="/images/mizzou_logo.svg"
+                alt="Mizzou Logo"
+                width={70}
+                height={70}
+                className={styles.headerImage}
+            />
             <span className={styles.headerText} onClick={handleHomeClick}>Roarin' Deals</span>
             <div className={styles.buttons}>
                 <HeaderButtons />
