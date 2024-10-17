b117aa25d5ca0f0d65ff3d8dc70ac0864f5079f2
ceegi <80815606+ceegi@users.noreply.github.com>
2024-10-17 15:49:08 -0500

    Update page.tsx

--- roarin-deals/src/app/Settings/page.tsx
+++ roarin-deals/src/app/Settings/page.tsx
@@ -2,7 +2,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Redirect to the onboarding page
+  // Redirect to the editAccount
   redirect('/settings/editAccount');
   return null; // This return is just to satisfy the return type
 }
