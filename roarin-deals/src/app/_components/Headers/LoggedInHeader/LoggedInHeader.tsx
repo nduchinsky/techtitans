4072d4dacbc973dfb8848379b859411dcf29937e
Megan Murphy <mlmurphy1124@gmail>
2024-11-22 23:01:31 -0600

    logout header working on mobile

--- roarin-deals/src/app/_components/Headers/LoggedInHeader/LoggedInHeader.tsx
+++ roarin-deals/src/app/_components/Headers/LoggedInHeader/LoggedInHeader.tsx
@@ -5,9 +5,11 @@ import styles from './LoggedInHeader.module.scss';
 import logo from '../../../../../public/images/RD_logo.svg'
 import Image from 'next/image';
 import { LoggedInHeaderButtons } from '../../Buttons/LogoutHeaderButtons/LoggedInHeaderButtons';
+import checkIfUserIsMobile from '../../../../../_utils/checkIfUserIsMobile';
 
 const LoggedInHeader = () => {
     const router = useRouter();
+    const isUserMobile = checkIfUserIsMobile(400);
 
     const handleHomeClick = () => {
         router.push('/');
@@ -15,12 +17,14 @@ const LoggedInHeader = () => {
 
     return(
         <div className={styles.headerContainer}>
-            <div className={styles.titleContainer}>
-                <span>
-                    <Image src={logo} alt="RD Logo" width={60} height={60} />
-                </span>
-                <span className={styles.headerText} onClick={handleHomeClick}>Roarin' Deals</span>
-            </div>
+            {!isUserMobile && (
+                <div className={styles.titleContainer}>
+                    <span>
+                        <Image src={logo} alt="RD Logo" width={60} height={60} />
+                    </span>
+                    <span className={styles.headerText} onClick={handleHomeClick}>Roarin' Deals</span>
+                </div>
+            )}
             <LoggedInHeaderButtons />
         </div>
     );
