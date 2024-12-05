9824d1affe645c5a91103c4d28fe45fab852c99d
Megan Murphy <mlmurphy1124@gmail>
2024-12-05 15:16:17 -0600

    caret and animation looking decent on laptop

--- roarin-deals/src/app/_components/Buttons/LogoutHeaderButtons/LoggedInHeaderButtons.tsx
+++ roarin-deals/src/app/_components/Buttons/LogoutHeaderButtons/LoggedInHeaderButtons.tsx
@@ -5,6 +5,7 @@ import styles from "./LoggedInHeaderButtons.module.scss";
 import { useAuth } from "../../../../../context/AuthContext";
 import { BsCaretDownFill } from "react-icons/bs";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const LoggedInHeaderButtons = () => {
   const router = useRouter();
@@ -33,21 +34,55 @@ export const LoggedInHeaderButtons = () => {
     router.push("/listings");
   };
 
-  const handleMyListingsClick = () => {
-    router.push("/settings/editAccount");
-  };
+  const dropdownVariants = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+      y: -150,
+      x: 100
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 15,
+      },
+    },
+    exit: {
+      scale: 0,
+      opacity: 0, 
+      y: -150,
+      x: 100,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.pfpFrame} onClick={handleProfileClick} />
       <BsCaretDownFill className={styles.dropDownIcon} onClick={handleDropdownClick} />
-      {dropdownVisible && (
-        <div className={styles.dropdownContainer}>
-          <button className={styles.dropdownButton} onClick={handleListingsClick}>View All Listings</button>
-          <button className={styles.dropdownButton} onClick={handleProfileClick}>Edit Profile</button>
-          <button className={styles.dropdownButton} onClick={handleLogoutClick}>Log Out</button>
-        </div>
-      )}
+      <AnimatePresence>
+        {dropdownVisible && (
+          <motion.div
+            variants={dropdownVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className={styles.dropdownContainer}
+          >
+            <BsCaretDownFill className={styles.dropdownCaret} />
+              <button className={styles.dropdownButton} onClick={handleListingsClick}>View All Listings</button>
+              <button className={styles.dropdownButton} onClick={handleProfileClick}>Edit Profile</button>
+              <button className={styles.dropdownButton} onClick={handleLogoutClick}>Log Out</button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
